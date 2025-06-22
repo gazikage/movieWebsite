0597d5210148d0723460765533f822996975a996
Frenchie <frenchieweb3@gmail.com>
2025-06-22 10:29:40 +0100

    updated json files

--- movie-website/vite.config.js
+++ movie-website/vite.config.js
@@ -1,21 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-
-// export default defineConfig({
-//   plugins: [react(), tailwindcss(),],
-// })
-
-
 import { defineConfig } from 'vite';
-import vercel from 'vite-plugin-vercel';
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
- 
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  server: {
-    port: process.env.PORT,
-  },
-  plugins: [vercel(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist'
+  }
 });
